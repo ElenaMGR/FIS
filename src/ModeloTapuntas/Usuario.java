4f3c885ad637998d610356d4d90f8b958ccd1928
elena <elena@elena-K55VM>
2016-06-18 17:41:22 +0200

    Creada clase usuario

--- src/ModeloTapuntas/Usuario.java
+++ src/ModeloTapuntas/Usuario.java
@@ -5,6 +5,9 @@
  */
 package ModeloTapuntas;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author aanaya
@@ -16,12 +19,59 @@ class Usuario {
     private boolean visibilidad = false;
     private boolean pendienteBaja = false;
     // incluir los demás atributos
-            
+    private String nombre;
+    private String telefono;
+    private String breveDescripcionPersonal;
+    private ArrayList<TipoTransaccion> preferenciaCobro;
  
 
     Usuario(String nombreUsuario, String contraseña, String direccionCorreo) {
       this.nombreUsuario= nombreUsuario;
       this.contraseña = contraseña;
       this.direccionCorreo = direccionCorreo;
+    }
+    
+    void modificarVisiblididad(boolean visibilidad){
+        this.visibilidad=visibilidad;
+    }
+    
+    void nuevoVehiculo (String matricula, String marca, String modelo, String color, int numeroPlazas, String categoria, String confor){
+        
+    }
+    
+    ArrayList<PlanAlquiler> obtenerPlanesQueCumplanRequisitos (String ciudadRecogida, GregorianCalendar fechaInicio, GregorianCalendar fechaFin){
+        
+    }
+    
+    void definirPlanAlquiler (String matricula, GregorianCalendar fechaInicio, GregorianCalendar fechaFin, String ciudadRecogida){
+        
+    }
+    
+    void eliminarVehiculo (String matricula){
+        
+    }
+    
+    void introducirPerfil (String nombre, String telefono, String breceDescripcion, ArrayList<TipoTransaccion> preferenciaCobro){
+        
+    }
+    
+    ArrayList<PlanAlquiler> obtenerPlanesAlquiler (){
+        
+    }
+    
+    ArrayList<String> consultarPerfil(){
+        
+    }
+    
+    void ofertarPlanAlquiler (GregorianCalendar fechaInicio, String matricula){
+        
+    }
+    
+    Vehiculo buscarVehiculo (String matricula){
+        
+    }
+    
+    PlanAlquiler buscarPlanAlquiler (GregorianCalendar fechaInicio, String matricula){
+        
     }
 }
