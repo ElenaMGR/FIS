56787860dc7a2514e05521fa85f75f92bda38419
elena <elena@elena-K55VM>
2016-06-18 17:59:48 +0200

    Modificada clase vehiculo

--- src/ModeloTapuntas/Vehiculo.java
+++ src/ModeloTapuntas/Vehiculo.java
@@ -5,10 +5,49 @@
  */
 package ModeloTapuntas;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import sun.util.calendar.Gregorian;
+
 /**
  *
  * @author elena
  */
 class Vehiculo {
+    private String marca;
+    private String modelo;
+    private String confor;
+    private int numeroPlazas;
+    private String color;
+    private String categoria;
+    private String matricula;
+    private ArrayList<PlanAlquiler> planesAlquiler;
     
+    Vehiculo(String matricula, String marca, String modelo, String color, int numeroPlazas, String categoria, String confor){
+        
+    }
+    
+    ArrayList<String> obtenerDatosVehiculo(){
+        return null;
+    }
+        
+    boolean estasDisponible (GregorianCalendar fechaInicio, GregorianCalendar fechaFin){
+        return false;
+    }
+    
+    void incluirPlanAlquiler(){
+        
+    }
+    
+    boolean comprobarEstadoAlquileres(){
+        return false;
+    }
+    
+    void eliminarVehiculoAlquileres(){
+        
+    }
+    
+    void obtenerMatricula(){
+        
+    }
 }
